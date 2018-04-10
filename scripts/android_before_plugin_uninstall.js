c364f260d865a06845f9961080adf2273ea4a89a
codonward <patrick@engineerapart.com>
2018-04-10 13:22:38 -0500

    Plugin uninstall needs to restore mainapp file to original location

--- scripts/android_before_plugin_uninstall.js
+++ scripts/android_before_plugin_uninstall.js
@@ -2,7 +2,13 @@ const path = require('path');
 const fs = require('fs-extra');
 const { getAndroidJavaSrcPath, getPackageName, getProjectName } = require('./utils');
 
-// this script needs to delete the MainApplication file that was added.
+const pluginPackage = 'com.rolamix.plugins.audioplayer';
+const pluginInstalledPackagePath = pluginPackage.replace(/\./g, path.sep);
+
+// this script needs to put back the MainApplication file that was moved.
+// Cordova needs to be able to remove the file, unfortunately. It seems to keep track
+// of files that were copied and even if the source file no longer exists, it marks
+// the path as occupied and refuses to allow you to reinstall the plugin.
 
 module.exports = function androidAfterPluginRemove(context) {
   const deferral = context.requireCordovaModule('q').defer();
@@ -11,11 +17,13 @@ module.exports = function androidAfterPluginRemove(context) {
     const projectName = getProjectName(context);
     const packageName = getPackageName(context);
     const javaTargetPath = getAndroidJavaSrcPath(context);
+    const installedPluginPath = path.resolve(javaTargetPath, pluginInstalledPackagePath);
 
     const packagePath = packageName.replace(/\./g, path.sep);
     const mainAppTarget = path.resolve(javaTargetPath, packagePath, `${projectName}.java`);
+    const pluginSource = path.resolve(installedPluginPath, 'MainApplication.java');
 
-    console.log('Removing generated MainApplication file: ', mainAppTarget);
+    console.log('Restoring generated MainApplication file to plugin directory: ', mainAppTarget, '\n', pluginSource);
 
     if (fs.existsSync(mainAppTarget)) {
       fs.unlinkSync(mainAppTarget);
